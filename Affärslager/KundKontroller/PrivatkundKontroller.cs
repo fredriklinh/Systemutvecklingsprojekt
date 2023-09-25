c53269719c54f761d6879eca26fbabc4ff754a54
Henric Johansson <s2107575@student.hb.se>
2023-09-25 17:18:08 +0200

    skapat kontrollers och några Empryo för vissa metoder

---
+++ Affärslager/KundKontroller/PrivatkundKontroller.cs
@@ -0,0 +1,32 @@
+﻿using Datalager;
+using Entiteter.Personer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Affärslager.KundKontroller
+{
+    public class PrivatkundKontroller
+    {
+        UnitOfWork unitOfWork = new UnitOfWork();
+
+        public void RegistreraPrivatKund(Privatkund pk)
+        {
+            unitOfWork.privatKundRepository.Create(pk);
+        }
+
+        public ICollection<Privatkund> SökPrivatKunder (string input)
+        {
+            return unitOfWork.privatKundRepository.SökPrivatKunder(input);
+        }
+
+        public ICollection<Privatkund> LäsPrivatKunder()
+        {
+            return unitOfWork.privatKundRepository.Read();
+        }
+
+
+    }
+}
---
+++ Affärslager/PrisKontroller.cs
@@ -0,0 +1,14 @@
+﻿using Datalager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Affärslager
+{
+    public class PrisKontroller
+    {
+        UnitOfWork unitOfWork = new UnitOfWork();
+    }
+}
