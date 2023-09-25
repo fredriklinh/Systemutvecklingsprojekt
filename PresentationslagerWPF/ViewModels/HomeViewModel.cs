b76ef17405ed98a6ff856654cc8aec44d1c0db1b
BonAxel <123560647+BonAxel@users.noreply.github.com>
2023-09-25 09:38:59 +0200

    ÄNDRING

--- PresentationslagerWPF/ViewModels/HomeViewModel.cs
+++ PresentationslagerWPF/ViewModels/HomeViewModel.cs
@@ -1,4 +1,5 @@
-﻿using PresentationslagerWPF.Models;
+﻿using PresentationslagerWPF.Commands;
+using PresentationslagerWPF.Models;
 using System.Windows.Input;
 
 namespace PresentationslagerWPF.ViewModels
@@ -8,6 +9,37 @@ namespace PresentationslagerWPF.ViewModels
         public string WelcomeMessage => "Welcome";
 
         public ICommand NavigateHuvudMenyCommand { get; }
+
+        //Användarnamn för ANVÄNDARE
+        private string användarnamn = null!;
+        public string Användarnamn { get => användarnamn; set { användarnamn = value; OnPropertyChanged(); } }
+
+        //Lösenord för ANVÄNDARE
+        private string lösenord = null!;
+        public string Lösenord { get => lösenord; set { lösenord = value; OnPropertyChanged(); } }
+
+
+        //Command för att trigga logga in och navigera mellan fönster. 
+        private ICommand loggaInCommand = null!;
+        public ICommand LoggaInCommand => loggaInCommand ??= loggaInCommand = new RelayCommand(() =>
+        {
+            //OBS. VÅR METOD FÖR ATT LOGGA IN HÄR
+            //Expedit e = bokningsKontroller.LoggaIn(Användarnamn, Lösenord);
+            //if (e != null)
+            //{
+            //    HuvudMenyViewModel HuvudMeny = new HuvudMenyViewModel(e);
+            //    bool result = windowService.ShowDialog(HuvudMeny);
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Misslyckad inloggning");
+            //}
+        });
+        // Stänger ner applikationen
+        private ICommand exitCommand = null!;
+        public ICommand ExitCommand =>
+        exitCommand ??= exitCommand = new RelayCommand(() => App.Current.Shutdown());
+
 
     }
 }
--- PresentationslagerWPF/ViewModels/MainViewModel.cs
+++ PresentationslagerWPF/ViewModels/MainViewModel.cs
@@ -31,35 +31,7 @@ namespace PresentationslagerWPF.ViewModels
             //OBS. VÅR KONTROLLER HÄR
             //bokningsKontroller = new BokningKontroller();
         }
-        //Användarnamn för ANVÄNDARE
-        private string användarnamn = null!;
-        public string Användarnamn { get => användarnamn; set { användarnamn = value; OnPropertyChanged(); } }
 
-        //Lösenord för ANVÄNDARE
-        private string lösenord = null!;
-        public string Lösenord { get => lösenord; set { lösenord = value; OnPropertyChanged(); } }
-
-
-        //Command för att trigga logga in och navigera mellan fönster. 
-        private ICommand loggaInCommand = null!;
-        public ICommand LoggaInCommand => loggaInCommand ??= loggaInCommand = new RelayCommand(() =>
-        {
-            //OBS. VÅR METOD FÖR ATT LOGGA IN 
-            //Expedit e = bokningsKontroller.LoggaIn(Användarnamn, Lösenord);
-            //if (e != null)
-            //{
-            //    HuvudMenyViewModel HuvudMeny = new HuvudMenyViewModel(e);
-            //    bool result = windowService.ShowDialog(HuvudMeny);
-            //}
-            //else
-            //{
-            //    MessageBox.Show("Misslyckad inloggning");
-            //}
-        });
-        // Stänger ner applikationen
-        private ICommand exitCommand = null!;
-        public ICommand ExitCommand =>
-        exitCommand ??= exitCommand = new RelayCommand(() => App.Current.Shutdown());
     }
 }
 
