6e5a32773cf6c01c6efe1603e0192650cddb98a2
Henric Johansson <s2107575@student.hb.se>
2023-09-26 16:23:28 +0200

    s

--- Affärslager/FakturaKontroller.cs
+++ Affärslager/FakturaKontroller.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Datalager;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace Affärslager
 {
     internal class FakturaKontroller
     {
+
+        UnitOfWork unitOfWork = new UnitOfWork();
     }
 }
