bad1cf597fd3e071fccea772b1c032f25ae3f29a
Daniel Noun <danielnoun21@gmail.se>
2023-10-05 14:26:32 +0200

    a

--- PresentationslagerWPF/PDF/CreatePDF.cs
+++ PresentationslagerWPF/PDF/CreatePDF.cs
@@ -23,18 +23,19 @@ namespace PDF
                 $"\nUtcheckningsdatum: {masterbokning.SlutDatum}" +
                 $"\nTotalpris: {totalpris}" +
                 $"\nTotalpris inklusive rabatt: {totalprisrabatt}";
-            
-            //foreach (Logi logi in logis)
-            //{
-            //    Label label2 = new Label(logi.Typen, 0, 0, 504, 500, Font.Helvetica, 18, TextAlign.Center);
-            //}
+
+            foreach (Logi logi in logis)
+            {
+                Label label2 = new Label(logi.Typen, 0, 0, 504, 500, Font.Helvetica, 18, TextAlign.Center);
+                page.Elements.Add(label2);
+            }
 
             //string combinedString = string.Join(",", logis);
             //string test = labelText + combinedString;
             Label label = new Label(labelText, 0, 0, 504, 500, Font.Helvetica, 18, TextAlign.Center);
             page.Elements.Add(label);
-            //page.Elements.Add(label2);
-
+            page.Elements.RelativeTo.HasFlag(PageOrientation.Portrait);
+            //page.Elements[1].RelativeTo(page.UnderlyingElements);
 
             document.Draw(Util.GetPath($"PDF/{masterbokning.BokningsNr}.pdf"));
         }
