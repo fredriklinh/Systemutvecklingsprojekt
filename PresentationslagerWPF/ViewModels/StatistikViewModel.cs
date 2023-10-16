f79b5ab5651cf9541de3b02c4ffe054b9095d8e2
Henric Johansson <s2107575@student.hb.se>
2023-10-16 10:31:07 +0200

    Lagt till navigation från huvudmeny + inne i statistikfönstret

--- PresentationslagerWPF/ViewModels/StatistikViewModel.cs
+++ PresentationslagerWPF/ViewModels/StatistikViewModel.cs
@@ -6,17 +6,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MindFusion.Charting.Wpf;
-
+using System.Windows.Input;
+using PresentationslagerWPF.Commands;
+using PresentationslagerWPF.Services;
+using ScottPlot;
+using System.Windows.Markup;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace PresentationslagerWPF.ViewModels
 {
-    public class StatistikViewModel: ObservableObject
+    public class StatistikViewModel : ObservableObject
     {
-
+        
+        #region NAVIGATION
         public StatistikViewModel(NavigationStore navigationStore, Användare användare)
         {
 
+            NavigateLoggaUtCommand = new NavigateCommand<LoggaInViewModel>(new NavigationService<LoggaInViewModel>(navigationStore, () => new LoggaInViewModel(navigationStore)));
+            TillbakaCommand = new NavigateCommand<HuvudMenyViewModel>(new NavigationService<HuvudMenyViewModel>(navigationStore, () => new HuvudMenyViewModel(navigationStore, användare)));
         }
+        public ICommand NavigateLoggaUtCommand { get; }
+
+        private ICommand exitCommand = null!;
+        public ICommand ExitCommand =>
+        exitCommand ??= exitCommand = new RelayCommand(() => App.Current.Shutdown());
+
+        public ICommand TillbakaCommand { get; }
+        #endregion
+
+        #region TEST
+        
+        
+        
+        #endregion
+
     }
+
+
 }
