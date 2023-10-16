66770069412bec840629185306478cfd82b51ec8
Henric Johansson <s2107575@student.hb.se>
2023-10-16 17:40:32 +0200

    a

--- PresentationslagerWPF/ViewModels/StatistikViewModel.cs
+++ PresentationslagerWPF/ViewModels/StatistikViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Entiteter.Personer;
+using Entiteter.Tjänster;
 using PresentationslagerWPF.Models;
 using PresentationslagerWPF.Stores;
 using System;
@@ -10,13 +11,19 @@ using System.Windows.Input;
 using PresentationslagerWPF.Commands;
 using PresentationslagerWPF.Services;
 using System.Windows.Markup;
-
+using LiveCharts;
+using System.Collections.ObjectModel;
+using Affärslager;
 
 namespace PresentationslagerWPF.ViewModels
 {
     public class StatistikViewModel : ObservableObject
     {
+        #region Kontrollers
         
+        StatistikKontroller statistikKontroller = new StatistikKontroller();       
+        #endregion
+
         #region NAVIGATION
         public StatistikViewModel(NavigationStore navigationStore, Användare användare)
         {
@@ -33,10 +40,54 @@ namespace PresentationslagerWPF.ViewModels
         public ICommand TillbakaCommand { get; }
         #endregion
 
-        #region TEST
-        
-        
-        
+        #region MasterBokning
+
+        private ObservableCollection<MasterBokning> masterbokningar = null!;
+        public ObservableCollection<MasterBokning> Masterbokningar { get => masterbokningar; set { masterbokningar = value; OnPropertyChanged(); } }
+
+        private ObservableCollection<MasterBokning> objekt = null!;
+        public ObservableCollection<MasterBokning> Objekt { get => objekt; set { objekt = value; OnPropertyChanged(); } }
+
+        private DateTime valtÅr = DateTime.Now;
+        public DateTime ValtÅr { get => valtÅr; set { valtÅr = value; OnPropertyChanged(); } }
+
+
+        private ObservableCollection<int> årtal = new ObservableCollection<int>
+        {
+            2020,
+            2021,
+            2022,
+            2023
+        };
+        public ObservableCollection<int> Årtal { get => årtal; set { årtal = value; OnPropertyChanged(); } }
+
+        private int selectedItemÅr;
+        public int SelectedItemÅr
+        {
+            get => selectedItemÅr;
+            set
+            {
+                selectedItemÅr = value; OnPropertyChanged();
+                Objekt = new ObservableCollection<MasterBokning>(statistikKontroller.HämtaAllaBokningar(SelectedItemÅr));
+                Totalen = Objekt.Count;
+
+
+            }
+        }
+        private int totalen;
+        public int Totalen {  get => totalen; set {  totalen = value; OnPropertyChanged();} }
+
+
+
+
+
+        //private int? År = ValtÅr.Year;
+        //public int År { get => år; set => år = value; }
+
+
+
+
+
         #endregion
 
     }
