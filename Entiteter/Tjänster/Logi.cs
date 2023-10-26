2cdd7bc2b15ee4876c672d847192f2485d84a2a8
Daniel Noun <danielnoun21@gmail.se>
2023-10-26 10:54:12 +0200

    aa

--- Entiteter/Tjänster/Logi.cs
+++ Entiteter/Tjänster/Logi.cs
@@ -26,10 +26,6 @@ namespace Entiteter.Tjänster
         public int Bäddar { get; set; }
         public bool Kök { get; set; }
 
-        //public virtual IList<PrislistaLogi> PrislistaLogi { get; set; } = new List<PrislistaLogi>();
-
-        public virtual IList<MasterBokning> MasterBokning { get; set; } = new List<MasterBokning>();
-
         [ForeignKey("LogiTyp")]
         public string Typen { get; set; }
         public virtual LogiTyp? LogiTyp { get; set; }
